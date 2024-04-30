c4b358300d87e37ac0ef096831ef51fb61d4b920
omotayosam <37814830+omotayosam@users.noreply.github.com>
2024-04-30 01:04:41 +0100

    Update data-extract-v2.py
    added if __name__ == '__main__': protection to prevent RuntimeError: An attempt has been made to start a new process before the current process has finished its bootstrapping phase.

--- data-extract-v2.py
+++ data-extract-v2.py
@@ -25,30 +25,31 @@ def process_files_in_parallel(files, folder_path, output_file):
             vocab.update(characters)
     return vocab
 
-folder_path = "openwebtext"
-output_file_train = "output_train.txt"
-output_file_val = "output_val.txt"
-vocab_file = "vocab.txt"
-
-files = xz_files_in_dir(folder_path)
-total_files = len(files)
-
-split_index = int(total_files * 0.9)  # 90% for training
-files_train = files[:split_index]
-files_val = files[split_index:]
-
-# Ensure output files are empty before appending
-open(output_file_train, 'w').close()
-open(output_file_val, 'w').close()
-
-# Process the training files
-vocab_train = process_files_in_parallel(files_train, folder_path, output_file_train)
-
-# Process the validation files
-vocab_val = process_files_in_parallel(files_val, folder_path, output_file_val)
-
-# Combine vocabularies (if needed) and write to vocab.txt
-vocab = vocab_train.union(vocab_val)
-with open(vocab_file, "w", encoding="utf-8") as vfile:
-    for char in sorted(vocab):
-        vfile.write(char + '\n')
+if __name__ == '__main__':
+    folder_path = "openwebtext"
+    output_file_train = "output_train.txt"
+    output_file_val = "output_val.txt"
+    vocab_file = "vocab.txt"
+    
+    files = xz_files_in_dir(folder_path)
+    total_files = len(files)
+    
+    split_index = int(total_files * 0.9)  # 90% for training
+    files_train = files[:split_index]
+    files_val = files[split_index:]
+    
+    # Ensure output files are empty before appending
+    open(output_file_train, 'w').close()
+    open(output_file_val, 'w').close()
+    
+    # Process the training files
+    vocab_train = process_files_in_parallel(files_train, folder_path, output_file_train)
+    
+    # Process the validation files
+    vocab_val = process_files_in_parallel(files_val, folder_path, output_file_val)
+    
+    # Combine vocabularies (if needed) and write to vocab.txt
+    vocab = vocab_train.union(vocab_val)
+    with open(vocab_file, "w", encoding="utf-8") as vfile:
+        for char in sorted(vocab):
+            vfile.write(char + '\n')
